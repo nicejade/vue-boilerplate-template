9d0aab5a7010567c38eeb70e220db3df5cb91192
jade.yang <jade.yang@dji.com>
2017-06-17 19:24:34 +0800

    feat(vuex): update vuex persistent storage 😊

--- src/store/types.js
+++ src/store/types.js
@@ -1,4 +1,8 @@
 export default {
+  /*
+    Remarks: Unified management of persistent storage related key。
+   */
+  STORAGE_USER_ID: 'VBT_USER_UI',
   RECORD_USERINFO: 'RECORD_USERINFO',
   GET_USERINFO: 'GET_USERINFO'
 }
