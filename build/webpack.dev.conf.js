302d220e7d2714ff56fb5351c3288548fe1cd7d6
jade.yang <yunjeff@163.com>
2017-07-20 18:23:58 +0800

    feat(config): update index.html to index.ejs 😊

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -32,7 +32,7 @@ module.exports = merge(baseWebpackConfig, {
     // https://github.com/ampedandwired/html-webpack-plugin
     new HtmlWebpackPlugin({
       filename: 'index.html',
-      template: 'index.html',
+      template: 'index.ejs',
       inject: true
     }),
     new FriendlyErrorsPlugin()
