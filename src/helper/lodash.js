46b733206ca158718e03030954cb95d87a27c2d4
jade.yang <yunjeff@163.com>
2017-08-01 17:12:12 +0800

    feat(optimizate): ptimize on demand loading about lodash 😄

--- src/helper/lodash.js
+++ src/helper/lodash.js
@@ -1,5 +1,11 @@
-// https://www.npmjs.com/package/lodash-webpack-plugin
-// https://www.npmjs.com/package/babel-plugin-lodash
+/*
+  Date: 2017-08-01
+  Desc: Modular Lodash builds without the hassle. You can freely add the method you need
+  Detail:
+    https://www.npmjs.com/package/lodash-webpack-plugin
+    https://www.npmjs.com/package/babel-plugin-lodash
+*/
+
 import _ from 'lodash'
 
 export default {
