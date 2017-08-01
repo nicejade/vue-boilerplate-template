15bda190eca45025370b3039d0b1b9126c00cdbf
nicejade <jeffyaisgreat@gmail.com>
2017-08-01 17:10:48 +0800

    feat(g)

---
+++ src/helper/lodash.js
@@ -0,0 +1,18 @@
+// https://www.npmjs.com/package/lodash-webpack-plugin
+// https://www.npmjs.com/package/babel-plugin-lodash
+import _ from 'lodash'
+
+export default {
+  clone: _.clone,
+  cloneDeep: _.cloneDeep,
+  debounce: _.debounce,
+  throttle: _.throttle,
+  find: _.find,
+  values: _.values,
+  size: _.size,
+  pick: _.pick,
+  isEmpty: _.isEmpty,
+  isFunction: _.isFunction,
+  isArray: _.isArray,
+  isDate: _.isDate
+}
