370e662c5d36f44d5799952de4a533b5280aa7cb
jeffjade <jeffygisgreat@gmail.com>
2017-10-20 23:54:59 +0800

    feat(update): optimization & add notes 😊

--- src/views/partials/Frame.vue
+++ src/views/partials/Frame.vue
@@ -27,6 +27,11 @@ export default{
   },
 
   created () {
+    /*
+      Desc: 请求用户信息;当然，如果你在登录时候返回，可删除此行。
+    */
+    this.$getUserInfo()
+
     this.initMenuList()
   },
 
@@ -37,6 +42,14 @@ export default{
 
   methods: {
     initMenuList () {
+      /*
+        Desc:
+          初始化侧边栏；当然，侧边栏配置一般由前后端共同作用;
+          如果你的需求也是如此，那么，在 vuex 的 action 发起请求，是一个不错的选择；
+          为了避免每次刷新界面都发起请求，应将 menuList 持久化存储（localStorage）
+          鉴于没个项目有所区别，这部分实现，在现有代码中已省略，相信您会处理的更好。
+        Date: update@2017-10-20
+      */
       let menuList = RoutesMapConfig.slice()
       menuList.forEach(item => {
         item.component && delete item.component
