816b9515692f8fef2d3718f04d799af75c3c11a9
nicejade <jeffygisgreat@gmail.com>
2017-11-06 11:33:01 +0800

    feat(404): modify the 404 page 😊

--- src/views/partials/NotFound.vue
+++ src/views/partials/NotFound.vue
@@ -1,62 +1,90 @@
 <template>
-  <div class="page-error">
-    <div class="box">
-      <div class="pic">
-        <img src="../../assets/images/404.png" alt=""></div>
-      <div class="info">
-        <h1>404</h1>
-        <h4>返回首页</h4>
-        <el-button type="primary" @click.native="back">抱歉，该页面不存在</el-button>
-      </div>
+  <div class="error-404">
+    <div class="error-404-body">
+      <el-card>
+        <div class="error-404-body-title">4<span><icon class="zero-icon" name="zero"></icon></span>4</div>
+        <p class="error-404-body-message">YOU&nbsp;&nbsp;LOOK&nbsp;&nbsp;LOST</p>
+        <div class="error-404-btns">
+          <el-button @click="onGoHomeClick" :plain="true" size="large" type="info">返回首页</el-button>
+          <el-button @click="onBackPageClick" :plain="true" size="large" ttype="info">返回上一页</el-button>
+        </div>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default{
-  name: 'not-found',
-  components: {
-  },
-  data () {
-    return {
-    }
-  },
+export default {
+  name: 'Error404',
+
   methods: {
-    back () {
+    onBackPageClick() {
+      this.$router.go(-1)
+    },
+    onGoHomeClick() {
       this.$router.push('/')
     }
   }
 }
 </script>
-<style lang="scss">
-.page-error {
-  color: #707473;
+
+<style type="text/css" lang="scss">
+@keyframes error-404-animation {
+  0% {
+    transform: rotateZ(0deg);
+  }
+  20% {
+    transform: rotateZ(-60deg);
+  }
+  40% {
+    transform: rotateZ(-10deg);
+  }
+  60% {
+    transform: rotateZ(50deg);
+  }
+  80% {
+    transform: rotateZ(-20deg);
+  }
+  100% {
+    transform: rotateZ(0deg);
+  }
+}
+.error-404-body {
+  width: 100%;
+  max-width: 600px;
+  height: 500px;
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%,-50%);
   text-align: center;
-  .box{
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    width: 360px;
-    -webkit-transform: translate(-50%, -50%);
-    -ms-transform: translate(-50%, -50%);
-    -o-transform: translate(-50%, -50%);
-    transform: translate(-50%, -50%);
+  padding: 20px 0;
+  margin-bottom: 40px;
+  .error-404-body-title{
+    text-align: center;
+    font-size: 8rem;
+    font-weight: 700;
+    color: #2d8cf0;
+    height: 13rem;
+    line-height: 13rem;
+    span{
+      display: inline-block;
+      color: #19be6b;
+      font-size: 8rem;
+      animation: error-404-animation 3s ease 0s infinite alternate;
+    }
+    .zero-icon{
+      width: 7rem;
+      height: 7rem;
+    }
   }
-  .pic{
-    float: left;
-  }
-  .info{
-    float: right;
-  }
-  img{
-    width: 157px;
-  }
-  h1 {
-    font-size: 64px;
-  }
-  h4 {
-    font-size: 16px;
-    margin-bottom: 20px;
+  .error-404-body-message {
+    display: block;
+    text-align: center;
+    font-size: 1rem;
+    font-weight: 500;
+    letter-spacing: 12px;
+    color: #dddde2;
   }
 }
 </style>
