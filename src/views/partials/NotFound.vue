15cb78fd4d99f5ecf526a8091ec792fd78591950
nicejade <jeffygisgreat@gmail.com>
2017-11-07 22:38:25 +0800

    feat(NotFound): interesting 404 page with css3😊

--- src/views/partials/NotFound.vue
+++ src/views/partials/NotFound.vue
@@ -2,11 +2,18 @@
   <div class="error-404">
     <div class="error-404-body">
       <el-card>
-        <div class="error-404-body-title">4<span><icon class="zero-icon" name="zero"></icon></span>4</div>
+        <div class="error-404-body-title">
+          <span class="left-four">4</span>
+          <span class="cener-zero"><icon class="zero-icon" name="zero"></icon></span>
+          <span class="right-four">4</span>
+        </div>
         <p class="error-404-body-message">YOU&nbsp;&nbsp;LOOK&nbsp;&nbsp;LOST</p>
         <div class="error-404-btns">
-          <el-button @click="onGoHomeClick" :plain="true" size="large" type="info">返回首页</el-button>
-          <el-button @click="onBackPageClick" :plain="true" size="large" ttype="info">返回上一页</el-button>
+          <el-button @click="onGoHomeClick" :plain="true" size="large" type="info">
+          返回首页
+          </el-button>
+          <el-button @click="onBackPageClick" :plain="true" size="large" ttype="info">返回上一页
+          </el-button>
         </div>
       </el-card>
     </div>
@@ -34,44 +41,93 @@ export default {
     transform: rotateZ(0deg);
   }
   20% {
-    transform: rotateZ(-60deg);
+    transform: rotateZ(60deg);
   }
   40% {
-    transform: rotateZ(-10deg);
+    transform: rotateZ(10deg);
   }
   60% {
-    transform: rotateZ(50deg);
+    transform: rotateZ(-50deg);
   }
   80% {
-    transform: rotateZ(-20deg);
+    transform: rotateZ(20deg);
   }
   100% {
     transform: rotateZ(0deg);
   }
 }
+
+@keyframes left-four-animation {
+  0% {
+    transform: translateX(0);
+  }
+  20% {
+    transform: translateX(0);
+  }
+  40% {
+    transform: translateX(0);
+  }
+  60% {
+    transform: translateX(-50px);
+  }
+  80% {
+    transform: translateX(-20px);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+@keyframes right-four-animation {
+  0% {
+    transform: translateX(0);
+  }
+  20% {
+    transform: translateX(60px);
+  }
+  40% {
+    transform: translateX(40px);
+  }
+  60% {
+    transform: translateX(0px);
+  }
+  80% {
+    transform: translateX(10px);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
 .error-404-body {
-  width: 100%;
+  width: 96%;
   max-width: 600px;
-  height: 500px;
   position: absolute;
   left: 50%;
   top: 50%;
-  transform: translate(-50%,-50%);
+  transform: translate(-50%, -50%);
   text-align: center;
   padding: 20px 0;
-  margin-bottom: 40px;
   .error-404-body-title{
     text-align: center;
-    font-size: 8rem;
+    font-size: 7rem;
     font-weight: 700;
     color: #2d8cf0;
     height: 13rem;
     line-height: 13rem;
-    span{
+    .cener-zero{
       display: inline-block;
       color: #19be6b;
-      font-size: 8rem;
       animation: error-404-animation 3s ease 0s infinite alternate;
+      margin: 0 -1rem;
+    }
+    .left-four{
+      margin: 0;
+      display: inline-block;
+      animation: left-four-animation 3s ease 0s infinite alternate;
+    }
+    .right-four{
+      margin: 0;
+      display: inline-block;
+      animation: right-four-animation 3s ease 0s infinite alternate;
     }
     .zero-icon{
       width: 7rem;
@@ -85,6 +141,18 @@ export default {
     font-weight: 500;
     letter-spacing: 12px;
     color: #dddde2;
+  }
+}
+
+@media (max-width:375px) {
+  .el-card__body{
+    padding: 10px 0;
+  }
+  .error-404-body{
+    .error-404-body-title{
+      height: 12rem;
+      font-size: 6rem;
+    }
   }
 }
 </style>
