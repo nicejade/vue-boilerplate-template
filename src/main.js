bbe1a7387024ab21e8972524a8a255569998a0bc
nicejade <jeffygisgreat@gmail.com>
2017-12-15 22:29:14 +0800

    feat(main): perfect the vue setting for production

--- src/main.js
+++ src/main.js
@@ -6,6 +6,13 @@ import App from './App'
 import store from './store/index'
 import router from './router'
 
+/* Configure whether to allow vue-devtools inspection. */
+Vue.config.devtools = process.env.NODE_ENV !== 'production'
+
+/* Suppress all Vue logs and warnings. */
+Vue.config.silent = process.env.NODE_ENV === 'production'
+
+/* Set this to false to prevent the production tip on Vue startup(2.2.0+). */
 Vue.config.productionTip = process.env.NODE_ENV !== 'production'
 
 /* eslint-disable no-new */
